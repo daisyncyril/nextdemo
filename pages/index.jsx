3faa248074004a4fafc08b7866615f12d7fc2935
Daisyn <daisyn@sjinnovation.com>
2022-03-03 18:32:04 +0530

    fix conflict on index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-import Footer from "../comps/Footer";
-import Navbar from "../comps/navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
